0ab8237deb0d12ee615d64efd90e238b5f1a2b78
D7703_07 <d7703_07@d7703-07ui-imac.local>
2018-03-29 16:18:44 +0900

    Prime Number Check

--- Prime Number Check/ViewController.swift
+++ Prime Number Check/ViewController.swift
@@ -8,13 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    
+    @IBOutlet weak var textF: UITextField!
+    @IBOutlet weak var lbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textF.delegate = self
     }
 
+    
+    @IBAction func CheckBtn(_ sender: Any) {
+        let number = Int(textF.text!)
+        var isPrime = true
+        
+        if number == 1{
+            isPrime = false
+        }
+        
+        if number != 2 && number != 1 {
+            for i in 2 ..< number! {
+                if number! % i == 0{
+                    isPrime = false
+                }
+            }
+        }
+        if isPrime == true{
+            lbl.text = "Prime Number"
+        }else{
+            lbl.text = "Not Prime Number"
+        }
+    }
+    
+    @IBAction func ResetBtn(_ sender: Any) {
+        textF.text = " "
+        lbl.text = " "
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
